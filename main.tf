4eb12fd7425375e791e1144c523ef1f0d8038e07
rmkprabhu <manikandaprabhuravi@gmail.com>
2024-12-21 15:46:18 +0000

    added

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 # main.tf
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
